14c8a963e6979ae78857d3737f00232bd1862143
binarycarpenter <gharris3@gmail.com>
2014-04-28 21:21:33 -0400

    node and data

--- js/node2048.js
+++ js/node2048.js
@@ -1,6 +1,13 @@
-function AI(gameManager, config) {
-  this.gameManager = gameManager;
+function AI(game, config) {
+  debugger;
+  this.gameManager = {game:game, AIrunning:true};
+
   this.config = this.defaultConfig;
+  if(config) {
+    for(prop in config) {
+      this.config[prop] = config[prop];
+    }
+  }
 
   this.scoreMap = {};
   var lastVal = 1;
@@ -23,26 +30,44 @@ AI.prototype.defaultConfig = {
   forcedMovePenalty: 4
 };
 
-AI.prototype.runAI = function(minTime) {
-  if(!this.gameManager.game.over && this.gameManager.AIrunning) {
-    this.gameManager.move(this.getBestMove(minTime));
-    var self = this;
-    setTimeout(function(){ self.runAI(minTime); }, 200); // allow ui to update
-  }
-  else {
-    this.gameManager.stopAI();
-  }
-};
-
 AI.prototype.runHeadless = function(minTime) {
-  if(!this.gameManager.game.over && this.gameManager.AIrunning) {
-    this.gameManager.game.move(this.getBestMove(minTime));
+  this.numMoves = 0;
+  this.totalDepth = 0;
+  while(!this.gameManager.game.over && this.gameManager.AIrunning) {
+    this.numMoves++;
     this.gameManager.game.computerMove();
+    if(this.config.minTime) minTime = this.config.minTime;
+    this.gameManager.game.move(this.getBestMove(minTime));
     if(!this.gameManager.game.movesAvailable()) {
       this.gameManager.game.over = true; // Game over!
     }
-    this.runHeadless(minTime);
+    if(this.numMoves % 100 === 0) console.log("grid after " + this.numMoves + " moves:\n" + this.gameManager.game.grid.toString(true));
   }
+};
+
+AI.prototype.getStats = function(minTime) {
+  var statString = this.gameManager.game.score + ",";
+  statString += this.getMaxTile() + ",";
+  statString += this.gameManager.game.grid.toString() + ",";
+  for(var prop in this.defaultConfig) {
+    statString += this.defaultConfig[prop] + ",";
+  }
+  statString += this.numMoves + ",";
+  if(this.config.minTime) statString += this.config.minTime + ",";
+  else statString += minTime + ',';
+  statString += (this.totalDepth / this.numMoves) + '\n';
+  return statString;
+};
+
+AI.prototype.getMaxTile = function() {
+  var maxTile = 0;
+  var grid = this.gameManager.game.grid;
+  for(var x = 0; x < grid.size; x++) {
+    for(var y = 0; y < grid.size; y++) {
+      if(grid.cells[x][y] && grid.cells[x][y].value > maxTile) maxTile = grid.cells[x][y].value;
+    }
+  }
+  return maxTile;
 };
 
 AI.prototype.getBestMove = function(minSearchTime) {
@@ -61,9 +86,10 @@ AI.prototype.getBestMove = function(minSearchTime) {
     // if this is the final iteration was cut off prematurely, don't trust the results
     if(bestMove === null || new Date().getTime() < finishTime) {
       bestMove = recursiveBestMove;
+      depth++;
     }
-    depth++;
   }
+  this.totalDepth += (depth - 1);
   return bestMove;
 };
 
@@ -424,16 +450,342 @@ Grid.prototype.serialize = function () {
   };
 };
 
-Grid.prototype.toString = function() {
+Grid.prototype.toString = function(alignPretty) {
   var ret = "";
   for(var y = 0; y < this.size; y++) {
-    ret += "(";
+    if(!alignPretty) ret += "(";
     for(var x = 0; x < this.size; x++) {
       var tile = this.cells[x][y];
       var val = tile? tile.value.toString() : "";
+      if(alignPretty) {
+        var prefix = true;
+        for(var i = val.length; i < 4; i++) {
+          val = prefix? " " + val : val + " ";
+          prefix = !prefix;
+        }
+      }
       ret += "[" + val + "]";
     }
-    ret += ")"
+    if(alignPretty) ret += "\n";
+    else ret += ")";
   }
   return ret;
 };
+
+function Game(grid, score, over, won, keepPlaying) {
+  this.grid        = grid;
+  this.score       = score;
+  this.over        = over;
+  this.won         = won;
+  this.keepPlaying = keepPlaying;
+  this.startTiles  = 2;
+  this.size        = this.grid.size;
+}
+
+// Return true if the game is lost, or has won and the user hasn't kept playing
+Game.prototype.isGameTerminated = function () {
+  if (this.over || (this.won && !this.keepPlaying)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// Set up the initial tiles to start the game with
+Game.prototype.addStartTiles = function () {
+  for (var i = 0; i < this.startTiles; i++) {
+    this.addRandomTile();
+  }
+};
+
+// Adds a tile in a random position
+Game.prototype.addRandomTile = function () {
+  if (this.grid.cellsAvailable()) {
+    var value = Math.random() < 0.9 ? 2 : 4;
+    var tile = new Tile(this.grid.randomAvailableCell(), value);
+
+    this.grid.insertTile(tile);
+  }
+};
+
+Game.prototype.computerMove = function() {
+  this.addRandomTile();
+};
+
+// Save all tile positions and remove merger info
+Game.prototype.prepareTiles = function () {
+  this.grid.eachCell(function (x, y, tile) {
+    if (tile) {
+      tile.mergedFrom = null;
+      tile.savePosition();
+    }
+  });
+};
+
+// Move a tile and its representation
+Game.prototype.moveTile = function (tile, cell) {
+  this.grid.cells[tile.x][tile.y] = null;
+  this.grid.cells[cell.x][cell.y] = tile;
+  tile.updatePosition(cell);
+};
+
+Game.prototype.move = function(direction) {
+  // 0: up, 1: right, 2: down, 3: left
+  var self = this;
+
+  if (this.isGameTerminated()) return false; // Don't do anything if the game's over
+
+  var cell, tile;
+
+  var vector     = this.getVector(direction);
+  var traversals = this.buildTraversals(vector);
+  var moved      = false;
+
+  if(!vector || !traversals || !traversals.x || !traversals.y) {
+    console.log("bad vector!");
+  }
+
+  // Save the current tile positions and remove merger information
+  this.prepareTiles();
+
+  // Traverse the grid in the right direction and move tiles
+  traversals.x.forEach(function (x) {
+    traversals.y.forEach(function (y) {
+      cell = { x: x, y: y };
+      tile = self.grid.cellContent(cell);
+
+      if (tile) {
+        var positions = self.findFarthestPosition(cell, vector);
+        var next      = self.grid.cellContent(positions.next);
+
+        // Only one merger per row traversal?
+        if (next && next.value === tile.value && !next.mergedFrom) {
+          var merged = new Tile(positions.next, tile.value * 2);
+          merged.mergedFrom = [tile, next];
+
+          self.grid.insertTile(merged);
+          self.grid.removeTile(tile);
+
+          // Converge the two tiles' positions
+          tile.updatePosition(positions.next);
+
+          // Update the score
+          self.score += merged.value;
+
+        } else {
+          self.moveTile(tile, positions.farthest);
+        }
+
+        if (!self.positionsEqual(cell, tile)) {
+          moved = true; // The tile moved from its original cell!
+        }
+      }
+    });
+  });
+  return moved;
+};
+
+Game.prototype.isLegalMove = function(direction) {
+  // 0: up, 1: right, 2: down, 3: left
+  var self = this;
+
+  if (this.isGameTerminated()) return false; // Don't do anything if the game's over
+
+  var cell, tile;
+
+  var vector     = this.getVector(direction);
+  var traversals = this.buildTraversals(vector);
+
+  if(!vector || !traversals || !traversals.x || !traversals.y) {
+    console.log("bad vector!");
+  }
+
+  moved = false;
+
+  // Traverse the grid in the right direction and move tiles
+  traversals.x.forEach(function (x) {
+    traversals.y.forEach(function (y) {
+      cell = { x: x, y: y };
+      tile = self.grid.cellContent(cell);
+
+      if (tile) {
+        var positions = self.findFarthestPosition(cell, vector);
+        var next      = self.grid.cellContent(positions.next);
+
+        if (next && next.value === tile.value) {
+          moved = true;
+        }
+        else if(!self.positionsEqual(positions.farthest, tile)){
+          moved = true;
+        }
+      }
+    });
+  });
+  return moved;
+};
+
+// Get the vector representing the chosen direction
+Game.prototype.getVector = function (direction) {
+  // Vectors representing tile movement
+  var map = {
+    0: { x: 0,  y: -1 }, // Up
+    1: { x: 1,  y: 0 },  // Right
+    2: { x: 0,  y: 1 },  // Down
+    3: { x: -1, y: 0 }   // Left
+  };
+
+  return map[direction];
+};
+
+// Build a list of positions to traverse in the right order
+Game.prototype.buildTraversals = function (vector) {
+  var traversals = { x: [], y: [] };
+
+  for (var pos = 0; pos < this.grid.size; pos++) {
+    traversals.x.push(pos);
+    traversals.y.push(pos);
+  }
+
+  if(!vector || !traversals || !traversals.x || !traversals.y) {
+    console.log("bad vector!");
+  }
+
+  // Always traverse from the farthest cell in the chosen direction
+  if (vector.x === 1) traversals.x = traversals.x.reverse();
+  if (vector.y === 1) traversals.y = traversals.y.reverse();
+
+  return traversals;
+};
+
+Game.prototype.findFarthestPosition = function (cell, vector) {
+  var previous;
+
+  // Progress towards the vector direction until an obstacle is found
+  do {
+    previous = cell;
+    cell     = { x: previous.x + vector.x, y: previous.y + vector.y };
+  } while (this.grid.withinBounds(cell) &&
+           this.grid.cellAvailable(cell));
+
+  return {
+    farthest: previous,
+    next: cell // Used to check if a merge is required
+  };
+};
+
+Game.prototype.movesAvailable = function () {
+  return this.grid.cellsAvailable() || this.tileMatchesAvailable();
+};
+
+// Check for available matches between tiles (more expensive check)
+Game.prototype.tileMatchesAvailable = function () {
+  var self = this;
+
+  var tile;
+
+  for (var x = 0; x < this.size; x++) {
+    for (var y = 0; y < this.size; y++) {
+      tile = this.grid.cellContent({ x: x, y: y });
+
+      if (tile) {
+        for (var direction = 0; direction < 4; direction++) {
+          var vector = self.getVector(direction);
+          var cell   = { x: x + vector.x, y: y + vector.y };
+
+          var other  = self.grid.cellContent(cell);
+
+          if (other && other.value === tile.value) {
+            return true; // These two tiles can be merged
+          }
+        }
+      }
+    }
+  }
+
+  return false;
+};
+
+Game.prototype.positionsEqual = function (first, second) {
+  return first.x === second.x && first.y === second.y;
+};
+
+Game.prototype.clone = function() {
+  var gameClone = new Game(this.grid.clone(), this.score, this.over, this.won, this.keepPlaying);
+  return gameClone;
+}
+
+// Represent the current game as an object
+Game.prototype.serialize = function () {
+  return {
+    grid:        this.grid.serialize(),
+    score:       this.score,
+    over:        this.over,
+    won:         this.won,
+    keepPlaying: this.keepPlaying
+  };
+};
+
+
+function Tile(position, value) {
+  this.x                = position.x;
+  this.y                = position.y;
+  this.value            = value || 2;
+
+  this.previousPosition = null;
+  this.mergedFrom       = null; // Tracks tiles that merged together
+}
+
+Tile.prototype.savePosition = function () {
+  this.previousPosition = { x: this.x, y: this.y };
+};
+
+Tile.prototype.updatePosition = function (position) {
+  this.x = position.x;
+  this.y = position.y;
+};
+
+Tile.prototype.clone = function() {
+  newTile = new Tile({ x: this.x, y: this.y }, this.value);
+  //newTile.previousPosition = { x: this.previousPosition.x, y: this.previousPosition.y };
+  //newTile.mergedFrom = { x: this.previousPosition.x, y: this.previousPosition.y };
+  return newTile;
+}
+
+Tile.prototype.serialize = function () {
+  return {
+    position: {
+      x: this.x,
+      y: this.y
+    },
+    value: this.value
+  };
+};
+
+Tile.prototype.toString = function() {
+  return this.x + " " + this.y + " " + this.value;
+};
+
+/*
+AI.prototype.defaultConfig = {
+  scoreMapMultiplier: 2.5,
+  addRecursiveScores: true,
+  evalWithWorst: true,
+  emptyCellInPathPenalty: 0,
+  lookAheadInEval: false,
+  forcedMovePenalty: 4
+}; */
+
+var fs = require('fs');
+var minTime = 100;
+var properties = [{}, {scoreMapMultiplier:2.1}, {scoreMapMultiplier:2}, {evalWithWorst:false}, {minTime:300}];
+var games = 1;
+while(true) {
+  for(var i = 0; i < properties.length; i++) {
+    console.log('Starting game ' + games + '\n');
+    var game = new Game(new Grid(4, false), 0, false, false, true);
+    var ai = new AI(game, properties[i]);
+    ai.runHeadless(minTime);
+    fs.appendFileSync("data.csv", ai.getStats(minTime));
+    games++;
+  }
+}
